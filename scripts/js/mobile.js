51ba4ee6638dab3e5757a945c7fde11ee3b6ff53
doesnotexists <wolgok03042@gmail.com>
2021-10-04 21:51:35 +0900

    Linux x86_64 추가

--- scripts/js/mobile.js
+++ scripts/js/mobile.js
@@ -1,4 +1,4 @@
-var filter = "win16|win32|win64|mac|macintel|linux";
+var filter = "win16|win32|win64|mac|macintel|Linux x86_64";
 if(navigator.platform){
 	if(filter.indexOf(navigator.platform.toLowerCase())<0){
 		location.href = "/scripts/htmls/mobile.html"
