4cc944574fdb2d03ca9c6caa8c1a15f211d744bd
doesnotexists <wolgok03042@gmail.com>
2021-10-04 19:14:49 +0900

    ^q^

--- scripts/js/mobile.js
+++ scripts/js/mobile.js
@@ -1,6 +1,6 @@
 var filter = "win16|win32|win64|mac|macintel";
 if(navigator.platform){
 	if(filter.indexOf(navigator.platform.toLowerCase())<0){
-		location.href = "/scripts/html/mobile.html"
+		location.href = "/scripts/htmls/mobile.html"
 	}
 }
