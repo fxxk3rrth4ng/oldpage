023a18f5aec03d2880703e777c811b79ee9bd50d
doesnotexists <wolgok03042@gmail.com>
2021-10-24 05:16:18 +0900

    ^q^

--- JS/dark.js
+++ JS/dark.js
@@ -2,5 +2,4 @@ const btn = document.querySelector(".btn-toggle");
 
 btn.addEventListener("click", function() {
     document.body.classList.toggle("dark-theme");
-    document.header.classList.toggle("dark-theme");
 });
